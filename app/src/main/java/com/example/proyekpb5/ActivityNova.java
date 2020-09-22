66c74cc39e193d102a7bc487a0d72f99cbe357fe
CC_service <novitanova426@gmail.com>
2020-09-22 07:58:11 +0700

    new activity

---
+++ app/src/main/java/com/example/proyekpb5/ActivityNova.java
@@ -0,0 +1,14 @@
+package com.example.proyekpb5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityNova extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nova);
+    }
+}
